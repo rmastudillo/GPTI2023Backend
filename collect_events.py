a2c540021fee908046f9d7cd109099002c791320
allisonaguilar <allison.aguilar@uc.cl>
2023-06-25 00:27:55 -0400

    add brand

--- collect_events.py
+++ collect_events.py
@@ -1,46 +1,60 @@
-import datetime as _dt
+
 from datetime import date
 from typing import Dict, Iterator
 import scraper as _scraper
 import json as _json
 from jumbo_categories import jumbo_categories
-from santaisabel_categories import santaisabel_categories
+from unimarc_categories import unimarc_categories
 
-
-def create_products_dict(categories_list, website_url, supermarket, selenium=None) -> Dict:
+def create_products_dict(categories_list, website_url, supermarket,categories_list_secondary, selenium=None) -> Dict:
     products = {}
 
     print("File parsing started...")
-    for category_subcategory in categories_list:
+    for category_subcategory, category_subcategory_second in zip(categories_list, categories_list_secondary):
+        # categories_list_secondary, categori2 y subcategori2 sirven para pasar de unimarc a jumbo
         category = category_subcategory[0]
+        category2 = category_subcategory_second[0]
         subcategories = category_subcategory[1:]
-
+        subcategories2 = category_subcategory_second[1:]
         category_dict = {}
-        # Jumbo scraper
-        for subcategory in subcategories:
+     
+        for subcategory, subcategory2 in zip(subcategories, subcategories2):
             soup = _scraper.get_soup(
                 website_url, category, subcategory, selenium)
-            raw_prices = soup.find_all(class_="product-sigle-price-wrapper")
             
-            price_list = [price.text for price in raw_prices]
-            raw_products = soup.find_all(class_="shelf-product-title-text")
-            products_list = [product.text for product in raw_products]
-            raw_images = soup.find_all("img")
+            if supermarket == "unimarc":
+                raw_products = soup.find_all(class_="Text_text__cB7NM Shelf_nameProduct__CXI5M Text_text--left__1v2Xw Text_text--flex__F7yuI Text_text--regular__KSs6J Text_text--sm__KnF3l Text_text--black__zYYxI Text_text__cursor--pointer__WZsQE Text_text--none__zez2n")
+                products_list = [product.text for product in raw_products]
+                raw_prices = soup.find_all(class_="Text_text__cB7NM Text_text--left__1v2Xw Text_text--flex__F7yuI Text_text--semibold__MukSj Text_text--md__H7JI_ Text_text--guardsman-red__wr1D8 Text_text__cursor--auto__cMaN1 Text_text--none__zez2n")
+                price_list = [price.text for price in raw_prices]
+                raw_images = soup.find_all(class_="Shelf_defaultImgStyle__ylyx2")
+                raw_brands = soup.find_all(class_="Text_text__cB7NM Shelf_brandText__sGfsS Text_text--left__1v2Xw Text_text--flex__F7yuI Text_text--semibold__MukSj Text_text--sm__KnF3l Text_text--black__zYYxI Text_text__cursor--pointer__WZsQE Text_text--none__zez2n")
+                raw_brands = soup.find_all(class_="shelf-product-brand")
+            else:
+                raw_products= soup.find_all(class_="shelf-product-title-text")
+                products_list = [product.text for product in raw_products]
+                raw_prices= soup.find_all(class_="product-sigle-price-wrapper")
+                price_list = [price.text for price in raw_prices]
+                raw_images=soup.find_all("img")
+                raw_brands = soup.find_all(class_="shelf-product-brand")
+                brand_list = [brand.text for brand in raw_brands]
+
             subcategory_dict = {}
-            for product, price, image in zip(products_list, price_list, raw_images):
+            for product, price, image, brand in zip(products_list, price_list, raw_images, brand_list):
                 price_string = ''.join(
                     caracter for caracter in price if caracter.isdigit())
                 price_num = int(price_string)
                 product_dict = {
                     "precio": price_num,
+                    "marca" : brand,
                     "supermercado": supermarket,
                     "url_imagen": image["src"]
                 }
                 subcategory_dict[product] = product_dict
                 
-            category_dict[subcategory] = subcategory_dict
+            category_dict[subcategory2] = subcategory_dict
 
-        products[category] = category_dict
+        products[category2] = category_dict
 
     print("File parsing completed!")
     print("Open products.json file to see the results")
@@ -48,10 +62,16 @@ def create_products_dict(categories_list, website_url, supermarket, selenium=Non
 
 
 if __name__ == '__main__':
-    products_jumbo = create_products_dict(jumbo_categories, "https://www.jumbo.cl", "jumbo", "selenium")
-    products_santaisabel = create_products_dict(jumbo_categories, "https://www.santaisabel.cl", "santa isabel", "selenium")
-    with open("products_jumbo.json", mode="w", encoding='utf8') as products_file:
+    products_jumbo = create_products_dict(jumbo_categories, "https://www.jumbo.cl", "jumbo", jumbo_categories)
+    with open("products_jumbo_3.json", mode="w", encoding='utf8') as products_file:
        _json.dump(products_jumbo, products_file, ensure_ascii=False)
 
-    with open("products_santaisabel.json", mode="w", encoding='utf8') as products_file:
-        _json.dump(products_santaisabel, products_file, ensure_ascii=False)
+
+    # products_santaisabel = create_products_dict(jumbo_categories, "https://www.santaisabel.cl", "santa isabel",jumbo_categories, "selenium")
+    # with open("products_santaisabel.json", mode="w", encoding='utf8') as products_file:
+    #     _json.dump(products_santaisabel, products_file, ensure_ascii=False)
+
+    # products_unimarc = create_products_dict(unimarc_categories, "https://www.unimarc.cl/category", "unimarc",jumbo_categories, "selenium")
+
+    # with open("products_unimarc.json", mode="w", encoding='utf8') as products_file:
+    #     _json.dump(products_unimarc, products_file, ensure_ascii=False)
---
+++ procesar.py
@@ -0,0 +1,58 @@
+import json
+
+# Importar la estructura
+from jumbo_categories import jumbo_categories
+
+def procesar_json(json_data):
+    productos_vistos = set()  # Conjunto para almacenar los productos vistos
+    productos_a_eliminar = []  # Lista para almacenar los productos a eliminar
+
+    for categoria, subcategorias in json_data.items():
+        for subcategoria, productos in subcategorias.items():
+            for producto, detalles in list(productos.items()):
+                
+                if producto in productos_vistos:
+                    productos_a_eliminar.append((categoria, subcategoria, producto))  # Agregar producto duplicado a la lista
+                    print("Producto eliminado porque está duplicado:")
+                    print(producto)
+                else:
+                    productos_vistos.add(producto)
+
+                if "pack" in producto.lower():
+                    productos_a_eliminar.append((categoria, subcategoria, producto))  # Agregar producto con "pack" a la lista
+                    print("Producto eliminado porque contiene 'pack':")
+                    print(producto)
+                
+                if detalles.get("precio", 0) == 0:
+                    productos_a_eliminar.append((categoria, subcategoria, producto))  # Agregar producto con precio 0 a la lista
+                    print("Producto eliminado por tener precio 0:")
+                    print(producto)
+
+    # eliminar productos
+    for categoria, subcategoria, producto in productos_a_eliminar:
+        if categoria in json_data and subcategoria in json_data[categoria] and producto in json_data[categoria][subcategoria]:
+            del json_data[categoria][subcategoria][producto]
+
+    return json_data
+
+# Cargar el JSON
+with open('products_jumbo_3.json', 'r') as file:
+    json_string = file.read()
+
+# Parsear el JSON
+data = json.loads(json_string)
+
+# Procesar el JSON
+data_procesada = procesar_json(data)
+
+# Convertir el JSON procesado de nuevo a un string
+json_modificado = json.dumps(data_procesada)
+
+# Especificar la ruta y nombre del archivo de salida
+ruta_archivo_salida = 'productos_jumbo_proc.json'
+
+# Guardar el JSON modificado en el archivo de salida
+with open(ruta_archivo_salida, 'w') as file:
+    file.write(json_modificado)
+
+print("JSON modificado guardado en:", ruta_archivo_salida)
